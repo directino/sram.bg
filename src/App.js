88b08b274da2fd6872e152a04bd2c63dee10cb87
Alex Mitev <direct@abv.bg>
2021-03-25 16:11:05 +0200

    Installed bootstrap and react-bootstrap. Created basic Navbar in the Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,28 @@
+import { Navbar, Form, Button } from 'react-bootstrap';
+import './Header.css';
+
 const Header = () => {
-    return(
-        <p>Header</p>
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Sram.bg - потребителят отвръща на удара</Navbar.Brand>
+            {/* <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav> */}
+            <Form inline style={{position: 'absolute', right: 0}}>
+                <Form.Control className="mr-sm-2" type="email" placeholder="Въведи имейл" />
+
+                <Form.Control className="mr-sm-2" type="password" placeholder="Парола" />
+
+                <Button className="mr-sm-2" variant="outline-light" type="submit">
+                    Вход
+                </Button>
+                <Button className="mr-sm-2" variant="outline-light" type="register">
+                    Регистрация
+                </Button>
+            </Form>
+        </Navbar>
     );
 }
 
