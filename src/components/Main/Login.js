ae2d59f086b9f92f8eacc05584fbe44003025428
Alex Mitev <direct@abv.bg>
2021-03-29 02:24:35 +0300

    Edited favicon and removed default logos

--- src/components/Main/Login.js
+++ src/components/Main/Login.js
@@ -4,51 +4,51 @@ import { useAuth } from "../../services/authService"
 import { useHistory, Link } from "react-router-dom"
 
 export default function Login() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const { login } = useAuth()
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(false)
-    const history = useHistory()
+        const emailRef = useRef()
+        const passwordRef = useRef()
+        const { login } = useAuth()
+        const [error, setError] = useState("")
+        const [loading, setLoading] = useState(false)
+        const history = useHistory()
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setError("")
-        setLoading(true)
-        login(emailRef.current.value, passwordRef.current.value)
-            .then(() => {
-                history.push('/');
-            })
-            .catch(setError("Неуспешен вход!"))
+        function handleSubmit(e) {
+            e.preventDefault()
+            setError("")
+            setLoading(true)
+            login(emailRef.current.value, passwordRef.current.value)
+                .then(() => {
+                    history.push('/');
+                })
+                .catch(setError("Неуспешен вход!"))
 
-        setLoading(false)
-    }
+        return setLoading(false)
+}
 
-    return (
-        <Container className="d-flex align-items-center justify-content-center"
-            style={{ minHeight: "50vh" }}>
-            <div className="w-100" style={{ maxWidth: "400px" }}>
-                <Card>
-                    <Card.Body>
-                        <h2 className="text-center mb-4">Вход</h2>
-                        {error && <Alert variant="danger">{error}</Alert>}
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group id="email">
-                                <Form.Label>Имейл</Form.Label>
-                                <Form.Control type="email" ref={emailRef} required></Form.Control>
-                            </Form.Group>
-                            <Form.Group id="password">
-                                <Form.Label>Парола</Form.Label>
-                                <Form.Control type="password" ref={passwordRef} required></Form.Control>
-                            </Form.Group>
-                            <Button disabled={loading} className="w-100" type="submit">Вход</Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
-                <div className="w-100 text-center mt-2">
-                    Не сте регистриран? Моля използвайте <Link to="/register">Регистрация</Link>!
+return (
+    <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "50vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Вход</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email">
+                            <Form.Label>Имейл</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required></Form.Control>
+                        </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Парола</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                        </Form.Group>
+                        <Button disabled={loading} className="w-100" type="submit">Вход</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Не сте регистриран? Моля използвайте <Link to="/register">Регистрация</Link>!
                 </div>
-            </div>
-        </Container>
-    );
+        </div>
+    </Container>
+);
 }
